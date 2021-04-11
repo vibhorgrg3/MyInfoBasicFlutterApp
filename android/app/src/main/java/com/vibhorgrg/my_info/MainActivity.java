7f1fdd3c6f188d2e4eaa46f6e024e9146152b141
Vibhor Garg <60567317+vibhorgrg3@users.noreply.github.com>
2021-04-12 01:48:37 +0530

    Second update (final)

---
+++ android/app/src/main/java/com/vibhorgrg/my_info/MainActivity.java
@@ -0,0 +1,6 @@
+package com.vibhorgrg.my_info;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,85 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal[300],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage(
-                    'https://images.unsplash.com/photo-1611095788646-86737a001141?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'),
+                  radius: 50, backgroundImage: AssetImage('images/Vibhor.png')),
+              Text('Vibhor Garg',
+                  style: TextStyle(
+                    fontFamily: 'Satisfy',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold),
               ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                // padding: EdgeInsets.all(10),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email_rounded,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      "+91 790 696 1460",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'SansScourcePro',
+                        color: Colors.teal.shade900,
+                      ),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                // padding: EdgeInsets.all(10),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email_rounded,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    "vibhorgrg3@gmail.com",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'SansScourcePro',
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
         ),
       ),
     );
   }
 }
+// Text(
+// "vibhorgrg3@gmail.com",
+// style: TextStyle(
+// fontSize: 20,
+// fontFamily: 'SansScourcePro',
+// color: Colors.teal.shade900,
+// ),
+
 // Row(
 // mainAxisAlignment: MainAxisAlignment.spaceBetween,
 // crossAxisAlignment: CrossAxisAlignment.stretch,
